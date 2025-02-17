9f0cfbdb67886bff9c758b41b6d48c65fe172fbd
CJ Lawson <clawson@MT06KW6PKW3.lan>
2025-02-16 21:17:06 -0600

    eslintrules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -36,6 +36,7 @@ const config = {
       },
     ],
     "@typescript-eslint/no-unsafe-assignment": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "drizzle/enforce-delete-with-where": [
       "error",
       {
