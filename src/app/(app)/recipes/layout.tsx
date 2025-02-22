34281fc3413c09424155bdf0a37dffee643ce6e4
CJ Lawson <clawson@MT06KW6PKW3.lan>
2025-02-21 20:52:31 -0600

    root layout to recipes page

---
+++ src/app/(app)/recipes/layout.tsx
@@ -0,0 +1,22 @@
+import "~/styles/globals.css";
+
+import { GeistSans } from "geist/font/sans";
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "BiteClub",
+  description: "All your recipes in one place",
+  icons: [{ rel: "icon", url: "/favicon.ico" }],
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en" className={`${GeistSans.variable}`}>
+      <body>
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+}
